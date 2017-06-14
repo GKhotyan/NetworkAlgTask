c0b8460ebbeef94c41da0595141c56fc7d3235df
GKhotyan <kraskovo@gmail.com>
2017-06-14 18:29:48 +0300

    test included

---
+++ src/test/java/com/gkhotyan/NetworkTest.java
@@ -0,0 +1,44 @@
+package com.gkhotyan;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+/**
+ * @author georgiy.hotiyan@masterdata.ru
+ */
+public class NetworkTest {
+
+    @Test
+    public void connectAndQueryTest() {
+        Network network = new Network(5);
+        network.connect(1 , 2);
+        assertTrue(network.query(1, 2));
+
+        network.connect(2 , 4);
+        assertTrue(network.query(1, 4));
+
+        assertFalse(network.query(1, 3));
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testWrongInitialization() {
+        Network network = new Network(-1);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testWrongConnection() {
+        Network network = new Network(5);
+        network.connect(1, 6);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testWrongQuery() {
+        Network network = new Network(5);
+        network.connect(1, 2);
+        network.connect(3, 4);
+        network.query(3 , 6);
+    }
+
+}
