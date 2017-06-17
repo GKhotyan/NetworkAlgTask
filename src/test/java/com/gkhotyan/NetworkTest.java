483ec3dc7d09376c4927ea548c76470a60b39609
GKhotyan <kraskovo@gmail.com>
2017-06-17 08:09:18 +0300

    from commandline

--- src/test/java/com/gkhotyan/NetworkTest.java
+++ src/test/java/com/gkhotyan/NetworkTest.java
@@ -25,7 +25,7 @@ public class NetworkTest {
 
     @Test(expected=IllegalArgumentException.class)
     public void testWrongInitialization() {
-        Network network = new Network(-4);
+        Network network = new Network(-6);
     }
 
     @Test(expected=IllegalArgumentException.class)
