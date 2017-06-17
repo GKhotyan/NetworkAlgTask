65195b5f6a478d0a0f39adeeea3ed3da19f7905d
GKhotyan <kraskovo@gmail.com>
2017-06-17 08:05:28 +0300

    little changes

--- src/test/java/com/gkhotyan/NetworkTest.java
+++ src/test/java/com/gkhotyan/NetworkTest.java
@@ -25,7 +25,7 @@ public class NetworkTest {
 
     @Test(expected=IllegalArgumentException.class)
     public void testWrongInitialization() {
-        Network network = new Network(-3);
+        Network network = new Network(-4);
     }
 
     @Test(expected=IllegalArgumentException.class)
